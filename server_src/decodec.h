a35a67817b1b348841f2bb45e3a04b2b4c773620
Joao Nagasava <jnagasava@usp.br>
2020-07-01 00:58:28 -0300

    add decodec

--- server_src/decodec.h
+++ server_src/decodec.h
@@ -1,30 +1,16 @@
- /**
-  * #include "codec.h"
+/**
+  * #include "decodec.h"
   * -----------------
   * 
-  * Biblioteca responsável por fazer a transcrição entre as strings manipuladas
-  * pelas Threads de Input com as Threads de Conexão, servindo de Codec entre
-  * o padrão de exibição com o padrão definido pelo protocolo utilizado entre
-  * o Cliente <-> Servidor.
+  * Biblioteca responsável por fazer o decoding da informacao recebida
+  * pelo cliente
   * 
-  * As funções de ENCODING são responsáveis por:
-  * | -- interpretar as mensagens recebidas pelo usuário.
-  * | -- processar o input baseado no tipo de operação desejado.
-  * | -- devolver os parâmetros do comando de maneira organizada.
-  * | -- garantir que as mensagens respeitam o protocolo estabelecido
-  * |    tanto no formato quando no tamanho máximo permitido.
-  * 
-  * @TODO
-  * As funções de DECODING ainda não foram implementadas para esta versão
-  * 
-  * Obs: Não cabe à codec.h fazer verificações de conexão, pois para ela só 
-  *      importa a manipulação de strings para envio ao servidor ou apresentação.
-  *      ao usuário via terminal. 
+  * OBS.: No momento, nao existe a validacao dos comandos recebidos,
+  * pois acredita-se que estao sendo tratados diretamente no cliente
+  *  
   */
-#ifndef CODEC_H
-    #define CODEC_H 1459
-
-    #include "chat.h"     /* DEFAULT_SERVER_HOST e DEFAULT_SERVER_PORT */
+#ifndef DECODEC_H
+    #define DECODEC_H 1459
     
     /**
      * Constantes e Macros
@@ -32,19 +18,14 @@
      * 
      * ACTION_*****
      * Valores utilizados para identificar a ação que o usuário deseja
-     * realizar durante cada input. A descrição de cada ação está descrita
-     * de forma detalhada na Thread de Input.
+     * realizar
      * 
-     * Para a codec.h basta ser capaz de mapear quais ACTIONS existem
-     * no programa.
      *  
     */
       // COMANDOS GERAIS
       #define ACTION_NONE               0
-      #define ACTION_CONNECT          300
       #define ACTION_DISCONNECT       301
       #define ACTION_QUIT             302
-      #define ACTION_HELP             303
       #define ACTION_PING             304
 
       // COMANDOS QUE ALTERAM ESTADO DO CLIENTE 
@@ -64,13 +45,8 @@
       #define ACTION_KICK            606
       #define ACTION_UNKICK          607
 
-      // MENSAGEMS SERVER -> CLIENT
-      #define ACTION_SERVERMSG       700
-      #define ACTION_CHANNELMSG      701
-
-
+      // CASOS DE SUCESSO E ERRO
       #define INVALID_PROTOCOL       0
-      #define SUCCESS               200
       
       #define ERR_NOSUCHNICK        401
       #define ERR_ERRONEUSNICKNAME  432
@@ -81,113 +57,232 @@
 
       #define ERR_CHANOPRIVSNEEDED  482
 
-
+      // MODE (INVITE)
+      #define INVITE_ONLY_ON        1
+      #define INVITE_ONLY_OFF       0
 
     /**
-     * ENCONDER FUNCTIONS
-     */
+     * DETECT ACTION FUNCTION
+    */
 
       /**
        * @function
-       * int dcd_detectar_act(const char* cmd);
-       * -------------------------------
+       * int detect_action(const char* cmd);
+       * -----------------------------------
        * 
-       * Utilizada para detectar se alguma ACTION_* foi disparada ao analisar
-       * a string input de comando recebida por parâmetro.
+       * Funcao que retorna a acao/comando que o cliente
+       * deseja executar
        * 
-       * Obs: Comandos obrigatóriamente são iniciados com '/' e a mesma
-       * deve estar localizada logo no primeiro BYTE de cmd, caso contrário a mesma
-       * será interpretada como uma ACTION_MESSAGE por DEFAULT.
+       * Parametros:
+       * -----------
+       *     cmd : const char*
+       *         comando (inteiro)
        * 
-       * cmd: ponteiro para a string à ser analisada como input de comando
-       * 
-       * return: algum dos códigos ACTION_* definindo qual era a ação pretendida.
-       */
-        int dcd_detectar_act(const char* cmd);
+       * Retorno:
+       * --------
+       *     int :
+       *         ACTION_DISCONNECT       301
+       *         ACTION_QUIT             302
+       *         ACTION_PING             304
+       *         ACTION_NICK             401
+       *         ACTION_JOIN             402
+       *         ACTION_MESSAGE          503
+       *         ACTION_LIST             504
+       *         ACTION_MODE             601  
+       *         ACTION_INVITE           602
+       *         ACTION_WHOIS            603
+       *         ACTION_MUTE             604
+       *         ACTION_UNMUTE           605
+       *         ACTION_KICK             606
+       *         ACTION_KICK             606
+       *         ACTION_NONE              0
+      */
+      int detect_action(const char* cmd);
 
+    /**
+     * DECODER FUNCTIONS
+    */
 
+      // *** Nickname ***
       /**
        * @function
-       * int dcd_encode_connect(const char* cmd, char* host, char* port);
-       * -------------------------------
+       * void decode_nickname(const char* cmd, char* nickname);
+       * ------------------------------------------------------
        * 
-       * Recebe e processa a string cmd referente à ACTION_CONNECT enviada pelo
-       * usuário. Tem como função definir quais valores devem ser salvos em
-       * host e port baseado no formato do input do usuário.
+       * Retorna via parametro (nickname) o nome do cliente
        * 
-       * Caso 1º: /connect
-       * | -- Nenhum valor foi passado, então é atribuído os valores definidos
-       * |    em DEFAULT_SERVER_HOST e DEFAULT_SERVER_PORT para os parâmetros.
-       * 
-       * Caso 2º: /connect <HOST_IP> <PORT>
-       * | -- Salva em host e port os dois próximos parâmetros enviados juntos
-       * |    à /connect (Obs: não valida se os valores válidos, pois isso será
-       * |    verificado durante o processo de conexão).
-       * 
-       * cmd: ponteiro para a string à ser analisada como input de comando
-       * 
-       * host: aponta para a string em que será salvo o HOST
-       * 
-       * port: aponta para a string em que será salvo o PORT
-       * 
-       * return: 1 caso algum dos parâmetros não tenha sido enviado corretamente.
-       *         0 caso tudo ocorra nos conformes.
-       */
-        int dcd_encode_connect(relay_chat* rc, const char* cmd, char* host, char* port);
+       * Parametros:
+       * -----------
+       *     cmd : const char*
+       *         comando (inteiro)
+       *     nickname : char*
+       *         nome do cliente
+      */
+      void decode_nickname(const char* cmd, char* nickname);
 
-      char* dcd_encode_nickname(relay_chat* rc, const char* cmd);
-
-      char* dcd_encode_join(relay_chat* rc, const char* cmd);
-
+      // *** Join ***
       /**
        * @function
-       * char** encode_message(&rc, raw_str, raw_str_len)
-       * -------------------------------
+       * void decode_join(const char* cmd, char* channel);
+       * -------------------------------------------------
        * 
-       * Utilizada na ACTION_MESSAGE para converter a string desejada para o 
-       * formato definido no protocolo RFC 1459. Caso a mensagem ultrapasse o limite
-       * de caracteres definido pelo protocolo a mesma é separada em multiplas 
-       * mensagens distintas.
+       * Retorna via parametro (channel) o nome do canal em que o cliente
+       * quer entrar
        * 
-       * raw_str: string que será codificada.
+       * Parametros:
+       * -----------
+       *     cmd : const char*
+       *         comando (inteiro)
+       *     channel : char*
+       *         nome do canal
+      */
+      void decode_join(const char* cmd, char* channel);
+
+      // *** Mode ***
+      /**
+       * @function
+       * void decode_mode(const char* cmd, bool* invite_mode);
+       * -----------------------------------------------------
        * 
-       * raw_str_len: tamanho de raw_str
+       * Retorna via parametro (invite_mode) o modo do canal (entrada por convite ou nao)
        * 
-       * return: array de strings que representam as mensagens codificadas a partir de
-       *         raw. O ponteiro NULL indica o fim do array e deve ser desalocado após 
-       *         seu uso.
-       */
-        char** dcd_encode_client_message(relay_chat* rc, const char* raw_str, int raw_str_len);
+       * Parametros:
+       * -----------
+       *     cmd : const char*
+       *         comando (inteiro)
+       *     invite_mode : bool*
+       *         INVITE_ONLY_ON  (1) -> entrada no canal somente por convite
+       *         INVITE_ONLY_OFF (0) -> entrada livre no canal 
+      */
+      void decode_mode(const char* cmd, bool* invite_mode);
 
-      char* dcd_encode_mode(relay_chat* rc, const char* cmd);
-      char* dcd_encode_invite(relay_chat* rc, const char* cmd);
-      char* dcd_encode_whois(relay_chat* rc, const char* cmd);
-      char* dcd_encode_mute(relay_chat* rc, const char* cmd);
-      char* dcd_encode_unmute(relay_chat* rc, const char* cmd);
-      char* dcd_encode_kick(relay_chat* rc, const char* cmd);
-      char* dcd_encode_unkick(relay_chat* rc, const char* cmd);
+      // *** Invite ***
+      /**
+       * @function
+       * void decode_invite(const char* cmd, char* invite_nickname);
+       * -----------------------------------------------------------
+       * 
+       * Retorna via parametro (invite_nickname) o nome do cliente a ser convidado
+       * 
+       * Parametros:
+       * -----------
+       *     cmd : const char*
+       *         comando (inteiro)
+       *     invite_nickname : char*
+       *         nome do cliente a ser convidado
+      */
+      void decode_invite(const char* cmd, char* invite_nickname);
 
+      // *** Whois ***
+      /**
+       * @function
+       * void decode_whois(const char* cmd, char* whois_nickname);
+       * ---------------------------------------------------------
+       * 
+       * Retorna via parametro (whois_nickname) o nickname do cliente o qual deseja
+       * saber o IP
+       * 
+       * Parametros:
+       * -----------
+       *     cmd : const char*
+       *         comando (inteiro)
+       *     whois_nickname : char*
+       *         nickame do cliente o qual sera pego o IP
+      */
+      void decode_whois(const char* cmd, char* whois_nickname);
 
-      int dcd_decode_nickname(relay_chat* rc, const char* cmd, char* nickname);
-      int dcd_decode_join(relay_chat* rc, const char* cmd, char* channel, bool* is_admin);
+      // *** Mute ***
+      /**
+       * @function
+       * void decode_mute(const char* cmd, char* mute_nickname);
+       * -------------------------------------------------------
+       * 
+       * Retorna via parametro (mute_nickname) o nickname do cliente o qual sera
+       * mutado
+       * 
+       * Parametros:
+       * -----------
+       *     cmd : const char*
+       *         comando (inteiro)
+       *     mute_nickname : char*
+       *         nickname do cliente o qual sera mutado
+      */
+      void decode_mute(const char* cmd, char* mute_nickname);
 
-      int dcd_decode_client_message(relay_chat* rc, const char* cmd, char* nickname, char* content);
-      int dcd_decode_list(relay_chat* rc, const char* cmd, char* channels);
+      // *** Unmute ***
+      /**
+       * @function
+       * void decode_unmute(const char* cmd, char* unmute_nickname);
+       * -----------------------------------------------------------
+       * 
+       * Retorna via parametro (unmute_nickname) o nickname do cliente o qual sera
+       * desmutado
+       * 
+       * Parametros:
+       * -----------
+       *     cmd : const char*
+       *         comando (inteiro)
+       *     unmute_nickname : char*
+       *         nickname do cliente o qual sera desmutado
+      */
+      void decode_unmute(const char* cmd, char* unmute_nickname);
 
-      int dcd_decode_mode(relay_chat* rc, const char* cmd);
-      int dcd_decode_whois(relay_chat* rc, const char* cmd, char* nickname, char* ip_info);
+      // *** Kick ***
+      /**
+       * @function
+       * void decode_kick(const char* cmd, char* kick_nickname);
+       * -------------------------------------------------------
+       * 
+       * Retorna via parametro (kick_nickname) o nickname do cliente o qual sera
+       * kikado do canal
+       * 
+       * Parametros:
+       * -----------
+       *     cmd : const char*
+       *         comando (inteiro)
+       *     kick_nickname : char*
+       *         nickname do cliente o qual sera kikado do canal
+      */
+      void decode_kick(const char* cmd, char* kick_nickname);
 
-      int dcd_decode_invite(relay_chat* rc, const char* cmd, char* nickname);
-      int dcd_decode_mute(relay_chat* rc, const char* cmd, char* nickname);
-      int dcd_decode_unmute(relay_chat* rc, const char* cmd, char* nickname);
-      int dcd_decode_kick(relay_chat* rc, const char* cmd, char* nickname);
-      int dcd_decode_unkick(relay_chat* rc, const char* cmd, char* nickname);
+      // *** Unkick ***
+      /**
+       * @function
+       * void decode_kick(const char* cmd, char* unkick_nickname);
+       * -------------------------------------------------------
+       * 
+       * Retorna via parametro (unkick_nickname) o nickname do cliente o qual sera
+       * deskikado do canal
+       * 
+       * Parametros:
+       * -----------
+       *     cmd : const char*
+       *         comando (inteiro)
+       *     unkick_nickname : char*
+       *         nickname do cliente o qual sera deskikado do canal
+      */
+      void decode_unkick(const char* cmd, char* unkick_nickname);
 
-      int dcd_decode_server_message(relay_chat* rc, const char* cmd,  char* content);
-      int dcd_decode_channel_message(relay_chat* rc, const char* cmd,  char* content);
-
-      int is_valid_channel_name(char* name);
-      int is_valid_nickname(char* name);
-
+      // *** Msg ***
+      /**
+       * @function
+       * void decode_msg(const char* cmd, char* role, char* nickname, char* message_content);
+       * --------------------------------------------------------------------------------------
+       * 
+       * Retorna via parametro o role (role) e o nickname (nickname) do cliente, e tambem a 
+       * mensagem (message_content) que ele enviou
+       * 
+       * Parametros:
+       * -----------
+       *     cmd : const char*
+       *         comando (inteiro)
+       *     role : char*
+       *         role do cliente (@ administrador | # usuario comum)
+       *     nickname : char*
+       *         nickname do cliente que enviou a mensagem
+       *     message_content : char*
+       *         mensagem que o cliente enviou    
+      */
+      void decode_msg(const char* cmd, char* role, char* nickname, char* message_content);
 
 #endif 
